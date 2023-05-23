2f23eeaa8d823460f724bea9d6ec0768812f6cda
OhnoHaruki <haruki20010219@gmail.com>
2023-05-23 11:43:34 +0900

    fix main_test.go

--- cmd/shortURLz/main_test.go
+++ cmd/shortURLz/main_test.go
@@ -2,12 +2,6 @@ package main
 
 import "testing"
 
-func Example_Main() {
-	goMain([]string{})
-	// Output:
-	// Hello World
-}
-
 func Test_Main(t *testing.T) {
 	if status := goMain([]string{"./shortURLz", "-v"}); status != 0 {
 		t.Error("Expected 0, got ", status)
